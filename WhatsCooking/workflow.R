afe7706fe90d3d1812914a8912656cce4d6d4d70
sp6740 <sp6740@byu.edu>
2025-12-09 16:42:00 -0700

    getting my final kaggle score

--- WhatsCooking/workflow.R
+++ WhatsCooking/workflow.R
@@ -3,6 +3,8 @@ library(tidymodels)
 library(tidyverse)
 library(tidytext)
 library(textrecipes)
+library(vroom)
+library(dplyr)
 trainSet <- read_file("C:/Users/sophi/OneDrive/Documents/Stat_348/Cooking/WhatsCooking/train.json") %>%
 fromJSON()
 testSet <- read_file("C:/Users/sophi/OneDrive/Documents/Stat_348/Cooking/WhatsCooking/test.json") %>%
@@ -20,66 +22,59 @@ fromJSON()
 # unnest(ingredients) %>%
 #   count(ingredients)
 
-my_recipe <- recipe(cuisine ~ ., data = trainSet) %>%
-  step_mutate(ingredients = tokenlist(ingredients)) %>%
-  step_tokenfilter(ingredients, max_tokens=3000) %>%
-  step_tfidf(ingredients)
+
+trainSet <- trainSet %>%
+  as_tibble() %>%
+  mutate(text_ingredients = map_chr(ingredients, ~ paste(.x, collapse = " ")),
+         num_ingredients = map_int(ingredients, length))
+
+testSet <- testSet %>%
+  as_tibble() %>%
+  mutate(text_ingredients = map_chr(ingredients, ~ paste(.x, collapse = " ")),
+         num_ingredients = map_int(ingredients, length))
+
+my_recipe <- recipe(cuisine ~ text_ingredients + num_ingredients, data = trainSet) %>%
+  step_tokenize(text_ingredients) %>%
+  step_tokenfilter(text_ingredients, max_tokens=500) %>%
+  step_tfidf(text_ingredients) %>%
+  step_normalize(all_numeric_predictors()) %>%
+  step_zv(all_predictors())
 prepped_recipe <- prep(my_recipe)
 baked <- bake(prepped_recipe, new_data=trainSet)
-  
 
-# my_model <- rand_forest(mtry=3,
-#                         trees=100,
-#                         min_n=5) %>%
-#   set_engine("ranger") %>%
-#   set_mode("classification")
-# 
-# my_wf <- workflow() %>%
-#   add_recipe(my_recipe) %>%
-#   add_model(my_model) %>%
-#   fit(data = trainSet)
-  
-# svm_spec <- svm_linear(cost = 1) %>%
-#   set_engine("LiblineaR") %>%
-#   set_mode("classification")
-# 
-# svm_wf <- workflow() %>%
-#   add_recipe(my_recipe) %>%
-#   add_model(svm_spec) %>%
-#   fit(data = trainSet)
 
-model <- multinom_reg(penalty = tune(), mixture = 1) %>% 
-  set_engine("glmnet") %>%
+
+my_model <- rand_forest(mtry=20,
+                        trees=300,
+                        min_n=5) %>%
+  set_engine("ranger") %>%
   set_mode("classification")
-
 
 my_wf <- workflow() %>%
   add_recipe(my_recipe) %>%
-  add_model(model)
-
-folds <- vfold_cv(trainSet, v = 5)
-
-grid <- grid_regular(penalty(range = c(-4, 1)),levels = 20)
-
-tuned <- tune_grid(my_wf,
-  resamples = folds,
-  grid = grid,
-  metrics = metric_set(accuracy))
-
-bestTune <- select_best(tuned, "accuracy")
+  add_model(my_model) %>%
+  fit(data = trainSet)
+  
 
 
-preds <- predict(bestTune, new_data = testSet)
+preds <- predict(my_wf, new_data = testSet)
 
 # preds <- predict(my_wf, new_data = testSet)
 
+testSet <- testSet %>%
+  rename(id = id)
+
 
 kaggle_submission <- preds %>%
-  bind_cols(testSet %>% select(id)) %>%
-  select(id, .pred_class) %>%
+  bind_cols(testSet %>% dplyr::select(id)) %>%
   rename(cuisine = .pred_class)
 
 vroom_write(x=kaggle_submission, file="./cuisinepreds.csv", delim=",")
+
+names(testSet)
+
+
+
 
 
   
